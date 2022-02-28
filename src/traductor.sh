bcc83081b860f97c058e0ecabb608927d5639f16
eleache <IKPEdLzwoD26aKE@gmail.com>
2022-02-28 06:20:12 -0600

    j

--- src/traductor.sh
+++ src/traductor.sh
@@ -1,19 +1,49 @@
-#!/bin.bash
-# author : eleAche
-# title: traductor para terminal
-# requirements:
-PDFTOTEXT='pdftotext '
-TRANS='translate-shell '
-POPPLER='poppler '
-
-if (( ${ls /usr/bin/share | grep -c $PDFTOTEXT } -eq 0 )); then
-  pacman -Syy $PDFTOTEXT || pacman -Syy $PDFTOTEXT || apt-get install -y $POPPLER || apt-get install -y $POPPLER;
+#!/bin/bash
+: '
+author : eleAche    
+title: traductor para terminal
+description:  haciendo uso de la utilidad pdftotext y trans (translate-shell)
+              es una serie de comandos que suelo introducir de forma manual
+              y ahora quiero convertirlo a un script para automatizar este proceso
+              repetitivo, pues se puede obtener mucho contenido educativo de
+              archivos .pdf; Usando google dorks, por ejemplo, haciendo la siguiente consulta: 
+                >_ "libro informatica" filetype:pdf
+              o usando metagoofil:
+                >_ metagoofil -d nmap.org -t pdf -l 200 -n 10 -o /tmp/ -f /tmp/resultados_mgf.html
+              y con esto poder romper las barreras del idioma, y asi poder aumentar
+              el numero de personas que llegan a ser verdaderamente habiles
+              en el area que se desenvuelvan.
+requirements:   
+'
+# pdftotext
+if [ ! `pdftotext -V cd` ]; then
+  apt-get -y install pdftotext || pacman -Sy pdftotext || echo 'Hay que ser root'
 fi
-
-if (( ${ls /usr/bin/share | grep -c $TRANSLATE-SHELL } -eq 0 )); then
-  pacman -Syy $TRANS || apt-get install -y $TRANS;
+# translate-shell
+if [ ! `trans -V cd` ]; then
+  apt-get -y install translate-shell || pacman -Sy translate-shell || echo 'Hay que ser root'
 fi
+: ' 
+se ejecuta en el directorio, enlista los archivos
+filtro por extension .pdf
+filtro por magicnumber correspondiente ( file -l )
+obten la lista de archivos validos (operandos)
 
-echo 'escribe la ruta al libro con extension .pdf  :'
-read LIBRO
-pdftotext $LIBRO libro.txt | trans -b :es
+'
+ls ~/textfiles || mkdir ~/textfiles
+declare WORKDIR="~/textfiles"
+cp *.pdf ~/textfiles;
+cd ~/textfiles
+for (( Listade=1; Listade++; Listade<73 )); do
+    pdftotext $Listade.pdf $Listade.txt
+    echo $Listade
+done
+rm -r *.pdf
+# echo `pwd` | cd;
+#echo "`ls | grep '.pdf'`" | awk -F. '{print $1}' | grep -n '' > $WORKDIR\/history.txt
+
+#for file in *.pdf; do mv $file $file.pdf; done
+
+#cat $WORKDIR\/history.txt | while read line; do
+#  echo $line > $WORKDIR/STACKFILE
+#done
